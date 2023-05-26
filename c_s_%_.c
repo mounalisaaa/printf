4b5dc175cb49efc09e1dec53a8bb9065c6c6b4cb
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-05-26 23:44:02 +0100

    p

---
+++ c_s_%_.c
@@ -0,0 +1,9 @@
+#include"main.h"
+int _printf(const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+
+	
+
+}
---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#include<stdarg.h>
+
+int _printf(const char *format, ...);
+
+
+
+
+
+#endif
